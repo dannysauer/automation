004d0bd00f32abeb18f2abac4a55a9c3eee99373
lcavajani <lcavajani@suse.com>
2019-03-07 16:15:50 +0100

    update etcd version test
    Signed-off-by: lcavajani <lcavajani@suse.com>

--- testinfra/acceptance-tests/etcd/test_etcd_version.py
+++ testinfra/acceptance-tests/etcd/test_etcd_version.py
@@ -4,18 +4,23 @@ import pytest
 @pytest.mark.bsc1121850
 @pytest.mark.common
 @pytest.mark.parametrize("package", ["etcd", "etcdctl"])
-def test_etcd_packages_version_is_superior_to_3_3_11(host, package):
+@pytest.mark.parametrize("min_version", ["3.3.11"])
+def test_etcd_packages_version_is_superior_to_3_3_11(host, package, min_version):
     etcd = host.package(package)
-    version = etcd.version.split("-")[0].split(".")
 
-    major = int(version[0])
-    minor = int(version[1])
-    patch = int(version[2])
+    host_version = etcd.version.split("-")[0].split(".")
+    host_major = int(host_version[0])
+    host_minor = int(host_version[1])
+    host_patch = int(host_version[2])
 
-    assert major >= 3
+    min_major = int(min_version.split(".")[0])
+    min_minor = int(min_version.split(".")[1])
+    min_patch = int(min_version.split(".")[2])
 
-    if major == 3:
-        assert minor >= 3
+    assert host_major >= min_major
 
-        if minor == 3:
-            assert patch >= 11
+    if host_major == min_major:
+        assert host_minor >= min_minor
+
+        if host_minor == min_minor:
+            assert host_patch >= min_patch
